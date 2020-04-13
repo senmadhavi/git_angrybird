11bafa14e5abce248ba853519a8cb5c4e65f87f3
Madhavi Sen <senmadhavi@gmail.com>
2020-04-13 12:09:25 +0530

    modified sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,11 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var plateform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
 }
 
 function setup(){
@@ -17,6 +19,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    plateform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    plateform.display();
 }
